74800f1f6b44fd0368da0cd703b1c714130bd012
Brian McKenna <bmckenna@atlassian.com>
2020-04-21 22:13:56 +1000

    Explicit types, no beans

--- samples/kofu-reactive-minimal/src/main/kotlin/com/sample/Application.kt
+++ samples/kofu-reactive-minimal/src/main/kotlin/com/sample/Application.kt
@@ -4,39 +4,43 @@ import org.springframework.boot.WebApplicationType
 import org.springframework.fu.kofu.application
 import org.springframework.fu.kofu.webflux.webFlux
 import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
+import reactor.core.publisher.Mono
 
 val app = application(WebApplicationType.REACTIVE) {
-	beans {
-		bean<SampleService>()
-		bean<SampleHandler>()
-	}
-	webFlux {
-		port = if (profiles.contains("test")) 8181 else 8080
-		router {
-			val handler = ref<SampleHandler>()
-			GET("/", handler::hello)
-			GET("/api", handler::json)
-		}
-		codecs {
-			string()
-			jackson()
-		}
-	}
+  webFlux {
+    port = 8080
+    router {
+      val service = SampleService()
+      val handler = SampleHandler(service)
+      GET("/", handler::hello)
+      GET("/api", handler::json)
+    }
+    codecs {
+      string()
+      jackson()
+    }
+  }
 }
 
 data class Sample(val message: String)
 
 class SampleService {
-	fun generateMessage() = "Hello world!"
+  fun generateMessage(): String = "Hello world!"
 }
 
-@Suppress("UNUSED_PARAMETER")
+
 class SampleHandler(private val sampleService: SampleService) {
-	fun hello(request: ServerRequest)= ok().bodyValue(sampleService.generateMessage())
-	fun json(request: ServerRequest) = ok().bodyValue(Sample(sampleService.generateMessage()))
+  @Suppress("UNUSED_PARAMETER")
+  fun hello(request: ServerRequest): Mono<ServerResponse> =
+    ok().bodyValue(sampleService.generateMessage())
+
+  @Suppress("UNUSED_PARAMETER")
+  fun json(request: ServerRequest): Mono<ServerResponse> =
+    ok().bodyValue(Sample(sampleService.generateMessage()))
 }
 
 fun main() {
-	app.run()
+  app.run()
 }
